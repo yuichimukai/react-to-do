b2bd6ddaf074237231154fde53e7a266c58da900
Y.M <66901428+yuichimukai@users.noreply.github.com>
2021-05-28 22:57:09 +0900

    fix component  and add new function

---
+++ src/components/IncompleteTodos.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const IncompleteTodos = (props) => {
+  const { todos, onClickComplete, onClickDelete } = props;
+  return (
+    <div className="imcomplete-area">
+      <p className="title">未完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            // mapでレンダリングする際は仮想DOMの関係でkeyを設定する必要がある
+            <div key={todo} className="list-row">
+              <li>{todo}</li>
+              <button onClick={() => onClickComplete(index)}>完了</button>
+              <button onClick={() => onClickDelete(index)}>削除</button>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
